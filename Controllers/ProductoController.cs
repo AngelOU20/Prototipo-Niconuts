bb3b182ca7493f19eb73fa1080bdc3dabeea5567
Julio Ucharima <julio_ucharima@usmp.pe>
2021-04-24 11:18:47 -0500

    cambio ProductoController

--- Controllers/ProductoController.cs
+++ Controllers/ProductoController.cs
@@ -3,5 +3,9 @@ namespace Prototipo_Niconuts.Controllers
     public class ProductoController
     {
         public int id{get;set;}
+
+        public string Nombre{get;set;}
+
+        public double precio{get;set;}
     }
 }
