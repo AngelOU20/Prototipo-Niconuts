deca1d65d1b23456afa5edc135ce95607f404d1a
Julio Ucharima <julio_ucharima@usmp.pe>
2021-05-01 17:23:22 -0500

    Creacion del Catalogo

---
+++ Models/Producto.cs
@@ -0,0 +1,10 @@
+namespace Prototipo_Niconuts.Models
+{
+    public class Producto
+    {
+        public int id{get;set;}
+        public string Nombre{get;set;}
+        public double Precio{get;set;}
+
+    }
+}
