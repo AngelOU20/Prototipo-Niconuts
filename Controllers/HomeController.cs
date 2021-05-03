a6ed4d385accc3365702f42ede3dca26479f6258
Julio Ucharima <julio_ucharima@usmp.pe>
2021-05-03 16:15:32 -0500

    Cambios

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -28,6 +28,11 @@ namespace Prototipo_Niconuts.Controllers
             return View();
         }
 
+        public IActionResult Contactenos()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
