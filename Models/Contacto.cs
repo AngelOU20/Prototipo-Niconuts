4b418accac806dc57e15caf03382dc0344291101
Sandro Baldera <sandro_baldera@usmp.pe>
2021-05-08 09:01:20 -0500

    Creación de Formulario Contacto

---
+++ Models/Contacto.cs
@@ -0,0 +1,13 @@
+namespace Prototipo_Niconuts.Models
+{
+    public class Contacto
+    {
+        public int id{get;set;}
+        public string Nombre{get;set;}
+        public double Apellido{get;set;}
+        public string Email{get;set;}
+        public string Telefono{get;set;}
+        public string Mensaje{get;set;}
+
+    }
+}
