9588521d582bd99de7f7681f1c6525f896e90df0
Julio Ucharima <julio_ucharima@usmp.pe>
2021-05-08 22:41:38 -0500

    Creacion Cliente en Models

---
+++ Models/Cliente.cs
@@ -0,0 +1,12 @@
+namespace Prototipo_Niconuts.Models
+{
+    public class Cliente
+    {
+        public int id{get;set;}
+        public string Nombre{get;set;}
+        public string Apellido{get;set;}
+        public string Email{get;set;}
+        public string Telefono{get;set;}
+
+    }
+}
