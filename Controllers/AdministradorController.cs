646fec7749937258a3ac4864260261a3530c29f6
Julio Ucharima <julio_ucharima@usmp.pe>
2021-05-09 14:41:13 -0500

    Creacion AdministradorController

---
+++ Controllers/AdministradorController.cs
@@ -0,0 +1,27 @@
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Prototipo_Niconuts.Data;
+
+namespace Prototipo_Niconuts.Controllers
+{
+    public class AdministradorController : Controller
+    {
+        private readonly ILogger<AdministradorController> _logger;
+        private readonly ApplicationDbContext _context;
+
+        public AdministradorController(ILogger<AdministradorController> logger,
+            ApplicationDbContext context)
+        {
+            _logger = logger;
+            _context = context;
+        }
+
+        public IActionResult Administrador()    //https://localhost:5001/Administrador/Administrador
+        {
+            var listcontactos = _context.DataContacto.ToList();
+            ViewData["Message"] = "";
+            return View(listcontactos);
+        }
+    }
+}
