ffa8a9d5d5c5a5309b874d12115d072c100bd275
Julio Ucharima <julio_ucharima@usmp.pe>
2021-05-10 15:43:17 -0500

    Creación Cliente

--- Models/Cliente.cs
+++ Models/Cliente.cs
@@ -1,8 +1,17 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace Prototipo_Niconuts.Models
 {
+    [Table("t_cliente")]
     public class Cliente
     {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Column("id")]
         public int id{get;set;}
+
+        [Required(ErrorMessage = "Por favor ingrese Nombre")]
+        [Display(Name="nombre")]
         public string Nombre{get;set;}
         public string Apellido{get;set;}
         public string Email{get;set;}
