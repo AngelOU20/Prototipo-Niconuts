60b351532c96a560fdba0109d7eb642fd7fbf4cd
Julio Ucharima <julio_ucharima@usmp.pe>
2021-05-10 15:43:50 -0500

    Creacion Producto

--- Models/Producto.cs
+++ Models/Producto.cs
@@ -1,11 +1,22 @@
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace Prototipo_Niconuts.Models
 {
+    [Table("t_producto")]
     public class Producto
     {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Column("id")]
         public int id{get;set;}
+
+        [Column("nombre")]
         public string Nombre{get;set;}
-        public double Precio{get;set;}
-        public string Foto{get;set;}
+
+        [Column("descripcion")]
+        public string Descripcion{get;set;}
+
+        public decimal Precio{get;set;}
+        public string Imagen{get;set;}
 
     }
 }
