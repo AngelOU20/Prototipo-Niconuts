f593afafe48a06e072485a97f09bb0df198ac35c
Julio Ucharima <julio_ucharima@usmp.pe>
2021-05-30 15:10:17 -0500

    Requerido

--- Models/Contacto.cs
+++ Models/Contacto.cs
@@ -1,3 +1,4 @@
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Prototipo_Niconuts.Models
@@ -9,18 +10,23 @@ namespace Prototipo_Niconuts.Models
         [Column("id")]
         public int id{get;set;}
 
+        [Required]
         [Column("nombre")]
         public string Nombre{get;set;}
 
+        [Required]
         [Column("apellido")]
         public string Apellido{get;set;}
 
+        [Required]
         [Column("email")]
         public string Email{get;set;}
 
+        [Required]
         [Column("telefono")]
         public string Telefono{get;set;}
 
+        [Required]
         [Column("mensaje")]
         public string Mensaje{get;set;}
 
