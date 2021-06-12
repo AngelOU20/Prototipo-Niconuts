d109965f703b80168c1719e0fe996888a1b09504
Julio Ucharima <julio_ucharima@usmp.pe>
2021-06-11 21:56:33 -0500

    Tercera Migracion

---
+++ Data/Migrations/20210611173441_thirdMigration.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Prototipo_Niconuts.Data.Migrations
+{
+    public partial class thirdMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<decimal>(
+                name: "SubTotal",
+                table: "proforma",
+                type: "numeric",
+                nullable: false,
+                defaultValue: 0m);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "SubTotal",
+                table: "proforma");
+        }
+    }
+}
